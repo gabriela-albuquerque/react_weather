8d26cb5dbd4dc3037592012b40f75ace31129600
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-22 18:10:26 -0300

    feita requisicao a api e atualizado o valor do estado weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [weather, setWeather] = useState({});
   const inputRef = useRef();
 
-  function searchCity() {
-    console.log(inputRef.current.value);
+  async function searchCity() {
+    const city = inputRef.current.value;
+    const key = "77fae7631bce866108573dce8ade401f";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
+
+    const apiInfo = await axios.get(url);
+    setWeather(apiInfo.data);
   }
 
   return (
