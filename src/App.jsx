d1aea329c620b6e74772c80f97dec1b892e3dd14
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-22 13:00:54 -0300

    inclusao e referenciacao do input para informar a cidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef();
+
+  function searchCity() {
+    console.log(inputRef.current.value);
+  }
+
   return (
     <div>
       <h1>DevClub Previsão do Tempo</h1>
+      <input ref={inputRef} type="text" placeholder="Digite o nome da cidade" />
+      <button onClick={searchCity}>Buscar</button>
     </div>
   );
 }
